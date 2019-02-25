aed1bae9ec9b6a2ae66f8b145125b8b0aedd5dff
David Schneider <david.schneider@bivab.de>
2019-02-25 16:24:26 +0100

    Rename getTreeAsString to parseString in some tests and simplify the code
    Co-Authored-By: Blaise Recurt <blaise.recurt@clearsy.com>
    Co-Authored-By: David Schneider <david.schneider@bivab.de>

--- bparser/src/test/java/de/be4/classicalb/core/parser/definitions/DefinitionsErrorsTest.java
+++ bparser/src/test/java/de/be4/classicalb/core/parser/definitions/DefinitionsErrorsTest.java
@@ -8,7 +8,6 @@ import org.junit.Test;
 import de.be4.classicalb.core.parser.BParser;
 import de.be4.classicalb.core.parser.exceptions.BCompoundException;
 import de.be4.classicalb.core.parser.node.Start;
-import util.Ast2String;
 
 public class DefinitionsErrorsTest {
 
@@ -16,7 +15,7 @@ public class DefinitionsErrorsTest {
 	public void checkForInvalidSubstitution() throws Exception {
 		String s = "MACHINE Definitions \n DEFINITIONS \n foo == BEGIN\n x=1 END \nEND";
 		try {
-			getTreeAsString(s);
+			parseString(s);
 			fail("Invalid substitution was not detected.");
 		} catch (BCompoundException e) {
 			System.out.println(e.getMessage());
@@ -29,7 +28,7 @@ public class DefinitionsErrorsTest {
 	public void checkAtSymbolInDefinitions() throws Exception {
 		String s = "MACHINE Definitions \n DEFINITIONS \n foo == BEGIN\n @ END \nEND";
 		try {
-			getTreeAsString(s);
+			parseString(s);
 			fail("Invalid substitution was not detected.");
 		} catch (BCompoundException e) {
 			System.out.println(e.getMessage());
@@ -42,7 +41,7 @@ public class DefinitionsErrorsTest {
 	public void checkForInvalidExpression() throws Exception {
 		String s = "MACHINE Definitions \n DEFINITIONS \n foo == 1 + \nEND";
 		try {
-			getTreeAsString(s);
+			parseString(s);
 			fail("Invalid definition was not detected.");
 		} catch (BCompoundException e) {
 			System.out.println(e.getMessage());
@@ -55,7 +54,7 @@ public class DefinitionsErrorsTest {
 		String s = "MACHINE Definitions \n DEFINITIONS \n foo == 1=1\n /* \n comment\n comment2\n comment3 \n */\n&& 1=1 \nEND";
 		System.out.println(s);
 		try {
-			getTreeAsString(s);
+			parseString(s);
 			fail("Invalid definition was not detected.");
 		} catch (BCompoundException e) {
 			System.out.println(e.getMessage());
@@ -67,7 +66,7 @@ public class DefinitionsErrorsTest {
 	public void checkForInvalidDefinition() throws Exception {
 		String s = "MACHINE Definitions \n DEFINITIONS \n foo == BEING x :=1 END \nEND";
 		try {
-			getTreeAsString(s);
+			parseString(s);
 			fail("Invalid substitution was not detected.");
 		} catch (BCompoundException e) {
 			System.out.println(e.getMessage());
@@ -81,7 +80,7 @@ public class DefinitionsErrorsTest {
 	public void checkForInvalidFormula() throws Exception {
 		String s = "MACHINE Definitions \n DEFINITIONS\n foo == \n 1+; \nEND";
 		try {
-			getTreeAsString(s);
+			parseString(s);
 			fail("Invalid formula was not detected.");
 		} catch (BCompoundException e) {
 			System.out.println(e.getMessage());
@@ -94,7 +93,7 @@ public class DefinitionsErrorsTest {
 	public void checkForInvalidFormula2() throws Exception {
 		String s = "MACHINE Definitions \n DEFINITIONS\n foo == \n 1=; \nEND";
 		try {
-			getTreeAsString(s);
+			parseString(s);
 			fail("Invalid formula was not detected.");
 		} catch (BCompoundException e) {
 			System.out.println(e.getMessage());
@@ -107,7 +106,7 @@ public class DefinitionsErrorsTest {
 	public void checkForInvalidFormula3() throws Exception {
 		String s = "MACHINE Definitions \n DEFINITIONS\n foo(xx) == (xx : OBJECTS -->(1..card(OBJECTS))\n; \nEND";
 		try {
-			getTreeAsString(s);
+			parseString(s);
 			fail("Invalid formula was not detected.");
 		} catch (BCompoundException e) {
 			System.out.println(e.getMessage());
@@ -117,17 +116,9 @@ public class DefinitionsErrorsTest {
 		}
 	}
 
-	private String getTreeAsString(final String testMachine) throws BCompoundException {
+	private void parseString(final String testMachine) throws BCompoundException {
 		// System.out.println("Parsing: \"" + testMachine + "\":");
 		final BParser parser = new BParser("testcase");
 		final Start startNode = parser.parse(testMachine, false);
-
-		// startNode.apply(new ASTPrinter());
-		final Ast2String ast2String = new Ast2String();
-		startNode.apply(ast2String);
-		final String string = ast2String.toString();
-		// System.out.println(string);
-		// System.out.println();
-		return string;
 	}
 }
