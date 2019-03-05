5d1868388b49be41fbd4c9866a1f34035833edb1
David Schneider <david.schneider@bivab.de>
2019-03-05 11:23:56 +0100

    Refactor PrettyMachinePrinterTest and make testPrettyPrint2 workin in IntelliJ
    Co-Authored-By: Blaise Recurt <blaise.recurt@clearsy.com>
    Co-Authored-By: David Schneider <david.schneider@bivab.de>

--- bparser/src/test/java/de/be4/classicalb/core/parser/prettyprinter/PrettyMachinePrinterTest.java
+++ bparser/src/test/java/de/be4/classicalb/core/parser/prettyprinter/PrettyMachinePrinterTest.java
@@ -1,35 +1,48 @@
 package de.be4.classicalb.core.parser.prettyprinter;
 
-import static org.junit.Assert.*;
+import de.be4.classicalb.core.parser.util.Utils;
+import org.junit.Test;
+import util.Helpers;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
-import org.junit.Test;
-
-import de.be4.classicalb.core.parser.util.Utils;
-import util.Helpers;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class PrettyMachinePrinterTest {
 
-	@Test
-	public void testPrettyPrint() {
-		final String testMachine = "MACHINE Test VARIABLES x,y INVARIANT x : INTEGER & y : INTEGER INITIALISATION x:= 1 || y := 2 OPERATIONS foo = skip; bar = skip END";
-		final String result1 = Helpers.getPrettyPrint(testMachine);
-		final String result2 = Helpers.getPrettyPrint(result1);
-		System.out.println(result1);
-		assertEquals(result1, result2);
-	}
+    @Test
+    public void testPrettyPrint() {
+        final String testMachine = "MACHINE Test\n" +
+                                           "VARIABLES x, y\n" +
+                                           "INVARIANT x:INTEGER & y:INTEGER\n" +
+                                           "INITIALISATION x := 1 || y := 2\n" +
+                                           "OPERATIONS\n" +
+                                           "foo = skip;\n" +
+                                           "bar = skip\n" +
+                                           "END";
+        final String result1 = Helpers.getPrettyPrint(testMachine);
 
-	@Test
-	public void testPrettyPrint2() throws FileNotFoundException, IOException {
-		final String filePath = "src/test/resources/prettyprinter/PrettyPrinter.mch";
-		final String testMachine = Utils.readFile(new File(filePath));
-		final String result1 = Helpers.getPrettyPrint(testMachine);
-		System.out.println(result1);
-		final String result2 = Helpers.getPrettyPrint(result1);
-		System.out.println(result1);
-		assertEquals(result1, result2);
-	}
+        assertFalse(result1.isEmpty());
+
+        assertEquals(testMachine, result1);
+    }
+
+    @Test
+    public void testPrettyPrint2() throws IOException, URISyntaxException {
+        final URI uri = this.getClass()
+                                .getResource("/prettyprinter/PrettyPrinter.mch")
+                                .toURI();
+        final File file = new File(uri);
+        final String testMachine = Utils.readFile(file);
+        final String result1 = Helpers.getPrettyPrint(testMachine);
+        final String result2 = Helpers.getPrettyPrint(result1);
+
+        assertFalse(result1.isEmpty());
+        assertFalse(result2.isEmpty());
+        assertEquals(result1, result2);
+    }
 }
