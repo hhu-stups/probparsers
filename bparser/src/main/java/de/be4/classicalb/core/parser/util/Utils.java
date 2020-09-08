e6471d489e87b7a95b398753ce6192061464fede
dgelessus <dgelessus@users.noreply.github.com>
2020-09-08 19:04:04 +0200

    Remove no longer needed case in BOM handling in Utils.readFile
    The Windows/Unix differences described in the comments come from
    differences in the default charset: on Unix systems it's almost always
    UTF-8, but on Windows it's usually a legacy codepage like Windows-1252.
    When decoding a UTF-8 BOM as Windows-1252, it results in the three
    Unicode characters listed in the second if condition. This can no
    longer happen, because readFile now always uses UTF-8 and no longer
    relies on the default charset.

--- bparser/src/main/java/de/be4/classicalb/core/parser/util/Utils.java
+++ bparser/src/main/java/de/be4/classicalb/core/parser/util/Utils.java
@@ -152,16 +152,8 @@ public final class Utils {
 		}
 
 		// remove utf-8 byte order mark
-		// replaceAll \uFEFF did not work for some reason
-		// apparently, unix like systems report a single character with the code
-		// below
 		if (!content.isEmpty() && Character.codePointAt(content, 0) == 65279) {
 			content = content.substring(1);
-		}
-		// while windows splits it up into three characters with the codes below
-		if (!content.isEmpty() && Character.codePointAt(content, 0) == 239 && Character.codePointAt(content, 1) == 187
-				&& Character.codePointAt(content, 2) == 191) {
-			content = content.substring(3);
 		}
 
 		return content.replaceAll("\r\n", "\n");
