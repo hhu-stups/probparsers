3cf2f80d2646ff259d9eb7a58767495a7278cd9c
dgelessus <dgelessus@users.noreply.github.com>
2021-10-21 13:49:21 +0200

    Remove AST pretty-printing from eventbalg tests

--- eventbalg/src/test/java/de/be4/eventbalg/parser/AbstractTest.java
+++ eventbalg/src/test/java/de/be4/eventbalg/parser/AbstractTest.java
@@ -2,7 +2,6 @@ package de.be4.eventbalg.parser;
 
 import de.be4.eventbalg.core.parser.BException;
 import de.be4.eventbalg.core.parser.EventBParser;
-import de.be4.eventbalg.core.parser.analysis.ASTPrinter;
 import de.be4.eventbalg.core.parser.node.Start;
 
 public class AbstractTest {
@@ -14,11 +13,6 @@ public class AbstractTest {
 		}
 
 		final EventBParser parser = new EventBParser();
-		final Start rootNode = parser.parse(input, debugOutput);
-		if (debugOutput) {
-			rootNode.apply(new ASTPrinter());
-		}
-
-		return rootNode;
+		return parser.parse(input, debugOutput);
 	}
 }
