3807591e394468e8ced3a258a7c419e5d2ac395c
dgelessus <dgelessus@users.noreply.github.com>
2021-10-21 13:32:48 +0200

    Remove use of PreParserASTPrinter from tests
    The result of the pretty-printing is not used or checked in any way, so
    there's no point in pretty-printing it at all.

--- bparser/src/test/java/de/be4/classicalb/core/parser/PreParserTest.java
+++ bparser/src/test/java/de/be4/classicalb/core/parser/PreParserTest.java
@@ -1,8 +1,6 @@
 package de.be4.classicalb.core.parser;
 
-import de.be4.classicalb.core.parser.visualisation.PreParserASTPrinter;
 import de.be4.classicalb.core.preparser.lexer.LexerException;
-import de.be4.classicalb.core.preparser.node.Start;
 import de.be4.classicalb.core.preparser.parser.Parser;
 import de.be4.classicalb.core.preparser.parser.ParserException;
 import org.junit.Test;
@@ -14,7 +12,7 @@ import java.io.PushbackReader;
 
 public class PreParserTest {
 
-	private static void getTreeAsString(final String testMachine)
+	private static void preparse(final String testMachine)
 			throws ParserException, LexerException, IOException {
 		final Parser parser = new Parser(
 				new PreLexer(
@@ -22,62 +20,61 @@ public class PreParserTest {
 								new InputStreamReader(
 										new ByteArrayInputStream(
 												testMachine.getBytes())), 99)));
-		final Start startNode = parser.parse();
-		startNode.apply(new PreParserASTPrinter());
+		parser.parse();
 	}
 
 	@Test
 	public void testSimple1() throws Exception {
 		final String testMachine = "DEFINITIONS blub == skip";
-		getTreeAsString(testMachine);
+		preparse(testMachine);
 	}
 
 	@Test
 	public void testSimple2() throws Exception {
 		final String testMachine = "DEFINITIONS blub == skip;bla==blub";
-		getTreeAsString(testMachine);
+		preparse(testMachine);
 	}
 
 	@Test
 	public void testParameters1() throws Exception {
 		final String testMachine = "DEFINITIONS blub(a,b) == skip";
-		getTreeAsString(testMachine);
+		preparse(testMachine);
 	}
 
 	@Test
 	public void testParameters2() throws Exception {
 		final String testMachine = "DEFINITIONS blub(a,b) == skip;\n\tbla(x,y) == x+y";
-		getTreeAsString(testMachine);
+		preparse(testMachine);
 	}
 
 	@Test
 	public void testComplete1() throws Exception {
 		final String testMachine = "MACHINE TestMachine VARIABLES xx DEFINITIONS blub == skip;bla(x,y)==blub END";
-		getTreeAsString(testMachine);
+		preparse(testMachine);
 	}
 
 	@Test
 	public void testComplete2() throws Exception {
 		final String testMachine = "MACHINE TestMachine VARIABLES xx DEFINITIONS blub == skip;bla==blub INVARIANT xx : NAT END";
-		getTreeAsString(testMachine);
+		preparse(testMachine);
 	}
 
 	@Test
 	public void testComments1() throws Exception {
 		final String testMachine = "/* comment1 */bla /* comment2 */ DEFINITIONS/* comment3 */ blub/* comment4 */ ==/* comment5 */ skip/* comment6 */";
-		getTreeAsString(testMachine);
+		preparse(testMachine);
 	}
 
 	@Test
 	public void testComments2() throws Exception {
 		final String testMachine = "/* comment1 * / */bla /* //comment2 */ **DEFINITIONS/* comment3 */ blub/* comment4 */ ==/* comment5 */ skip/* ;comment6 */";
-		getTreeAsString(testMachine);
+		preparse(testMachine);
 	}
 
 	@Test
 	public void testComments3() throws Exception {
 		final String testMachine = "/* comment1 * / */bla /* //comment2 */ **DEFINITIONS/* comment3 */ blub/* comment4 */ ==/* comment5 */ skip/* comment6 */; bla == x:=5";
-		getTreeAsString(testMachine);
+		preparse(testMachine);
 	}
 
 }
