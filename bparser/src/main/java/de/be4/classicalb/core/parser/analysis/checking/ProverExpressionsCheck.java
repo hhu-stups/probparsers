5d5a7d75e85a85110d6493ed4a9835f5c95257a0
dgelessus <dgelessus@users.noreply.github.com>
2021-11-03 15:04:28 +0100

    Remove commented out checks from ProverExpressionsCheck

--- bparser/src/main/java/de/be4/classicalb/core/parser/analysis/checking/ProverExpressionsCheck.java
+++ bparser/src/main/java/de/be4/classicalb/core/parser/analysis/checking/ProverExpressionsCheck.java
@@ -7,7 +7,6 @@ import de.be4.classicalb.core.parser.ParseOptions;
 import de.be4.classicalb.core.parser.analysis.OptimizedTraversingAdapter;
 import de.be4.classicalb.core.parser.exceptions.CheckException;
 import de.be4.classicalb.core.parser.node.AProverComprehensionSetExpression;
-import de.be4.classicalb.core.parser.node.ASubstitutionPredicate;
 import de.be4.classicalb.core.parser.node.Start;
 
 /**
@@ -30,28 +29,11 @@ public class ProverExpressionsCheck extends OptimizedTraversingAdapter implement
 	public void setOptions(ParseOptions options) {
 		this.options = options;
 	}
-	
-// we allow #truth_predicate and bfalse cannot be used anyway as it is tokenize
-// so: why not allow it?
-// 	@Override
-// 	public void caseAFalsityPredicate(AFalsityPredicate node) {
-// 		exceptions.add(new CheckException("bfalse is not allowed in ordinary B files", node));
-// 	}
 
 	/* todo: ask Jens */
 	@Override
 	public void caseAProverComprehensionSetExpression(AProverComprehensionSetExpression node) {
 		exceptions.add(new CheckException("SET not allowed in ordinary B files", node));
-	}
-
-	@Override
-	public void caseASubstitutionPredicate(ASubstitutionPredicate node) {
-		// if (error == null) {
-		// error = new
-		// CheckException("Substitution in Predicates are not supported in
-		// ordinary B files",
-		// node);
-		// }
 	}
 
 	@Override
