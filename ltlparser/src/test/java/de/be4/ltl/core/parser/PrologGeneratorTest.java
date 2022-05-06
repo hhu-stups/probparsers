2d1119fb88d4ada94b15a2240422e3e0776791d5
dgelessus <dgelessus@users.noreply.github.com>
2022-05-06 13:50:16 +0200

    Simplify whitespace workaround in PrologGeneratorTest

--- ltlparser/src/test/java/de/be4/ltl/core/parser/PrologGeneratorTest.java
+++ ltlparser/src/test/java/de/be4/ltl/core/parser/PrologGeneratorTest.java
@@ -637,28 +637,16 @@ public class PrologGeneratorTest {
 		private void parse(final IPrologTermOutput pto, final String text,
 				final boolean wrap, final String wrapper)
 				throws ProBParseException {
-			int startOffset = -1;
-			for (int i = 0; i < text.length(); i++) {
-				final char ch = text.charAt(i);
-				if (startOffset == -1) {
-					// Skip leading whitespace automatically added by PrologGeneratorHelper.parsePredicateToken
-					if (Character.isSpaceChar(ch)) {
-						continue;
-					} else {
-						startOffset = i;
-					}
-				}
+			final String trimmedText = text.trim();
+			for (int i = 0; i < trimmedText.length(); i++) {
+				final char ch = trimmedText.charAt(i);
 				if (!Character.isLowerCase(ch) && "()[]{}".indexOf(ch) == -1)
 					throw new ProBParseException("syntax error");
 			}
-			if (startOffset == -1) {
-				startOffset = 0;
-			}
-
 			if (wrap) {
 				pto.openTerm(wrapper);
 			}
-			pto.printAtom(text.substring(startOffset));
+			pto.printAtom(trimmedText);
 			if (wrap) {
 				pto.closeTerm();
 			}
