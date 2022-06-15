e21595f8321178a08bc3eaa327a284f6e215012a
dgelessus <dgelessus@users.noreply.github.com>
2022-06-15 10:47:19 +0200

    Clean up PrologTerm atom(ic) string conversion API a bit
    Added a new set of clearly named methods for converting either just
    atoms or any atomic term, and deprecated the old misleadingly named
    methods. Also made the single-term methods non-static so they are easier
    to call.

--- prologlib/src/main/java/de/prob/prolog/term/PrologTerm.java
+++ prologlib/src/main/java/de/prob/prolog/term/PrologTerm.java
@@ -50,6 +50,10 @@ public abstract class PrologTerm {
 		return false;
 	}
 
+	public boolean isAtomic() {
+		return this.getArity() == 0 && !this.isVariable();
+	}
+
 	public boolean hasFunctor(final String functor, final int arity) {
 		return false;
 	}
@@ -85,28 +89,90 @@ public abstract class PrologTerm {
 		//	return arguments[index - 1];
 	}
 
-    // this should actually have been called atomString; we should rename this to atomGetString
-	public static String atomicString(final PrologTerm term) {
-		if (term.isAtom())
-			return term.getFunctor();
-		else
-			throw new IllegalArgumentException("Expected a Prolog atom, but was " + term.toString());
-	}
-	
-	// a variation of the above which also accepts numbers
-	public static String atomicGetString(final PrologTerm term) {
-		if (term.isAtom() || term.isNumber())
-			return term.getFunctor();
-		else
-			throw new IllegalArgumentException("Expected an atomic Prolog term, but was " + term.toString());
+	/**
+	 * Check that this term is an atom (0-argument compound term) and return its value (functor) as a string.
+	 * 
+	 * @return the atom's functor as a string
+	 * @throws IllegalArgumentException if this term is not an atom
+	 */
+	public String atomToString() {
+		if (this.isAtom()) {
+			return this.getFunctor();
+		} else {
+			throw new IllegalArgumentException("Expected a Prolog atom, but was " + this + " (" + this.getClass() + ")");
+		}
 	}
 
-	public static List<String> atomicStrings(final Iterable<PrologTerm> terms) {
-		List<String> result = new ArrayList<String>();
+	/**
+	 * Shorthand for {@link #atomToString()} on all terms in a list.
+	 * 
+	 * @param terms the atoms to convert to strings
+	 * @return the atoms' functors as strings
+	 * @throws IllegalArgumentException if any term in the list is not an atom
+	 */
+	public static List<String> atomsToStrings(final Iterable<? extends PrologTerm> terms) {
+		final List<String> result = new ArrayList<>();
 		for (final PrologTerm term : terms) {
-			result.add(atomicString(term));
+			result.add(term.atomToString());
 		}
 		return result;
+	}
+
+	/**
+	 * Check that this term is atomic (non-variable and has no arguments) and return its value (functor) as a string.
+	 * 
+	 * @return the term's functor as a string
+	 * @throws IllegalArgumentException if this term is not atomic
+	 */
+	public String atomicToString() {
+		if (this.isAtomic()) {
+			return this.getFunctor();
+		} else {
+			throw new IllegalArgumentException("Expected an atomic Prolog term, but was " + this + " (" + this.getClass() + ")");
+		}
+	}
+
+	/**
+	 * Shorthand for {@link #atomicToString()} on all terms in a list.
+	 * 
+	 * @param terms the terms to convert to strings
+	 * @return the terms' functors as strings
+	 * @throws IllegalArgumentException if any term in the list is not atomic
+	 */
+	public static List<String> atomicsToStrings(final Iterable<? extends PrologTerm> terms) {
+		final List<String> result = new ArrayList<>();
+		for (final PrologTerm term : terms) {
+			result.add(term.atomicToString());
+		}
+		return result;
+	}
+
+	/**
+	 * Identical to {@link #atomToString()}.
+	 * This method is badly named - it only accepts atoms and not other atomic terms (like numbers).
+	 * 
+	 * @param term the atom to convert to a string
+	 * @return the atom's functor as a string
+	 * @throws IllegalArgumentException if this term is not an atom
+	 * @deprecated Use {@link #atomToString()} or {@link #atomicToString()} instead.
+	 */
+	@Deprecated
+	public static String atomicString(final PrologTerm term) {
+		return term.atomToString();
+	}
+
+	/**
+	 * Identical to {@link #atomsToStrings(Iterable)}.
+	 * This method is badly named - it only accepts atoms and not other atomic terms (like numbers).
+	 * 
+	 * @param terms the terms to convert to strings
+	 * @return the atoms' functors as strings
+	 * @throws IllegalArgumentException if this term is not an atom
+	 * @deprecated Use {@link #atomsToStrings(Iterable)} or {@link #atomicsToStrings(Iterable)} instead.
+	 */
+	@Deprecated
+	public static List<String> atomicStrings(final Iterable<PrologTerm> terms) {
+		return atomsToStrings(terms);
 	}
 
 }
