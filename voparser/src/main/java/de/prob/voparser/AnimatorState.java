666b3cca63119a4ca597de20ea7afe4e06854042
dgelessus <dgelessus@users.noreply.github.com>
2022-09-28 13:02:59 +0200

    Remove voparser subproject
    Now pulled out into its own separate repo:
    https://gitlab.cs.uni-duesseldorf.de/general/stups/vo_parser

--- voparser/src/main/java/de/prob/voparser/AnimatorState.java
+++
@@ -1,5 +0,0 @@
-package de.prob.voparser;
-
-public enum AnimatorState {
-	TRACE, STATE_SPACE;
-}
--- voparser/src/main/java/de/prob/voparser/VTType.java
+++
@@ -1,7 +0,0 @@
-package de.prob.voparser;
-
-public enum VTType {
-	RELOAD, RESET, STATIC,
-	TRACE, STATE_SPACE,
-	CHECKING_PROP, EXPLORING_STATE_SPACE
-}
