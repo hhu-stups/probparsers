a8bc2fc37216908f083bc0b69d9dba9bf0844e65
dgelessus <dgelessus@users.noreply.github.com>
2022-10-11 16:10:00 +0200

    Make EPreplCommands order consistent with other code

--- cliparser/src/main/java/de/prob/cliparser/EPreplCommands.java
+++ cliparser/src/main/java/de/prob/cliparser/EPreplCommands.java
@@ -1,15 +1,26 @@
 package de.prob.cliparser;
 
 public enum EPreplCommands {
-	machine, formula, expression, predicate, substitution, 
-	extendedexpression, extendedpredicate, extendedsubstitution,
-	halt, 
-	definition, resetdefinitions,
-	ltl, ctl,
-	version, gitsha, shortversion,
-	extendedformula,
-	
-	// new commands to change parsingBehaviour, analog to command-line switches		
+	version,
+	shortversion,
+	gitsha,
+	definition,
+	resetdefinitions,
+	fastprolog,
+	compactpos,
 	verbose,
-	fastprolog, compactpos, checkname, lineno;
+	checkname,
+	lineno,
+	machine,
+	formula,
+	expression,
+	predicate,
+	substitution,
+	extendedformula,
+	extendedexpression,
+	extendedpredicate,
+	extendedsubstitution,
+	ltl,
+	ctl,
+	halt,
 }
