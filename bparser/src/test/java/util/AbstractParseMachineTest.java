51e4f2f3ab7eb372ef841096d5b89709179655fc
dgelessus <dgelessus@users.noreply.github.com>
2023-05-08 14:28:43 +0200

    Simplify AbstractParseMachineTest

--- bparser/src/test/java/util/AbstractParseMachineTest.java
+++ bparser/src/test/java/util/AbstractParseMachineTest.java
@@ -1,33 +1,17 @@
 package util;
 
 import java.io.File;
-import java.io.FilenameFilter;
 import java.net.URISyntaxException;
+import java.util.stream.Stream;
 
 public abstract class AbstractParseMachineTest {
-	
-
-	private static final class MachineFilenameFilter implements FilenameFilter {
-		private static final String[] MACHINE_SUFFIX = { ".mch", ".imp", ".ref", ".def" };
-
-		@Override
-		public boolean accept(final File dir, final String name) {
-			for (int i = 0; i < MACHINE_SUFFIX.length; i++) {
-				if (name.endsWith(MACHINE_SUFFIX[i])) {
-					return true;
-				}
-			}
-			return false;
-		}
-	}
-
 	protected static File[] getMachines(String path) {
 		final File dir;
 		try {
-			dir = new File(AbstractParseMachineTest.class.getClassLoader().getResource(path).toURI());
+			dir = new File(AbstractParseMachineTest.class.getResource("/" + path).toURI());
 		} catch (URISyntaxException e) {
 			throw new RuntimeException(e);
 		}
-		return dir.listFiles(new MachineFilenameFilter());
+		return dir.listFiles((d, name) -> Stream.of(".mch", ".imp", ".ref", ".def").anyMatch(name::endsWith));
 	}
 }
