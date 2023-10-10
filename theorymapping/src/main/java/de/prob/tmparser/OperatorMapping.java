b366b7015c1ab08c398a8a453423cc895d26180d
dgelessus <dgelessus@users.noreply.github.com>
2023-10-10 19:03:55 +0200

    Parse expressions in theory mappings before sending them to Prolog
    This allows using B expressions in .ptm files without probcli having to
    call the parser itself (which currently isn't possible in the Rodin
    plugin).

--- theorymapping/src/main/java/de/prob/tmparser/OperatorMapping.java
+++ theorymapping/src/main/java/de/prob/tmparser/OperatorMapping.java
@@ -1,5 +1,9 @@
 package de.prob.tmparser;
 
+import de.be4.classicalb.core.parser.BParser;
+import de.be4.classicalb.core.parser.analysis.prolog.ASTProlog;
+import de.be4.classicalb.core.parser.exceptions.BCompoundException;
+import de.be4.classicalb.core.parser.node.Start;
 import de.prob.prolog.output.IPrologTermOutput;
 
 public class OperatorMapping {
@@ -7,6 +11,7 @@ public class OperatorMapping {
 	private final String operatorName;
 	private final TMOperatorType operatorType;
 	private final String spec;
+	private final Start parsedSpec;
 
 	public OperatorMapping(String theoryName, String operatorName,
 			TMOperatorType operatorType, String spec) {
@@ -15,6 +20,19 @@ public class OperatorMapping {
 		this.operatorName = operatorName;
 		this.operatorType = operatorType;
 		this.spec = spec;
+
+		// TODO Should we handle this in the actual parser as a different token/node type?
+		if (spec.startsWith("$") && spec.endsWith("$")) {
+			String formula = spec.substring(1, spec.length() - 1);
+			try {
+				this.parsedSpec = new BParser().parseExpression(formula);
+			} catch (BCompoundException exc) {
+				throw new TheoryMappingException("Parse error in B expression for operator " + operatorName + ": " + exc.getMessage(), exc);
+			}
+		} else {
+			// Spec doesn't contain a B expression
+			this.parsedSpec = null;
+		}
 	}
 
 	public String getTheoryName() {
@@ -31,6 +49,15 @@ public class OperatorMapping {
 
 	public String getSpec() {
 		return spec;
+	}
+
+	/**
+	 * Get a parsed AST for the expression (if any) in the spec.
+	 * 
+	 * @return B expression AST parsed from the spec, or {@code null} if the spec doesn't contain an expression
+	 */
+	public Start getParsedSpec() {
+		return this.parsedSpec;
 	}
 
 	@Override
@@ -84,7 +111,14 @@ public class OperatorMapping {
 	public void printProlog(IPrologTermOutput pout) {
 		pout.openTerm("tag");
 		pout.printAtom(this.getOperatorName());
-		pout.printAtom(this.getSpec());
+		if (this.getParsedSpec() != null) {
+			// For B expressions, send the parsed AST so that the Prolog side doesn't have to call the parser again
+			pout.openTerm("bexpr");
+			ASTProlog.printFormula(this.getParsedSpec(), pout);
+			pout.closeTerm();
+		} else {
+			pout.printAtom(this.getSpec());
+		}
 		pout.closeTerm();
 	}
 }
