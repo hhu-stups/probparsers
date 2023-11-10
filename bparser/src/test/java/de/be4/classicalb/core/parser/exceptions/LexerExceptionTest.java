add3361efd45155503215da09327f8fb924e93c8
dgelessus <dgelessus@users.noreply.github.com>
2023-11-10 16:34:14 +0100

    Add error position tests with CRLF (Windows) newlines

--- bparser/src/test/java/de/be4/classicalb/core/parser/exceptions/LexerExceptionTest.java
+++ bparser/src/test/java/de/be4/classicalb/core/parser/exceptions/LexerExceptionTest.java
@@ -14,10 +14,22 @@ public class LexerExceptionTest {
 		Assert.assertTrue(e.getFirstException().getLocations().get(0).getFilename().contains("LexerStringError.mch"));
 	}
 
+	@Test
+	public void testLexerExceptionWindows() {
+		final BCompoundException e = Assert.assertThrows(BCompoundException.class, () -> Helpers.parseFile("exceptions/LexerStringError_win.mch"));
+		Helpers.assertParseErrorLocation(e, 3, 12, 3, 12);
+		Assert.assertTrue(e.getFirstException().getLocations().get(0).getFilename().contains("LexerStringError_win.mch"));
+	}
 
 	@Test
 	public void testLexerThrowsExceptionAndProvidesPositionInfo() {
 		final BCompoundException b = Assert.assertThrows(BCompoundException.class, () -> Helpers.parseFile("exceptions/IfAndPredicates.mch"));
+		Helpers.assertParseErrorLocation(b, 18, 18, 18, 18);
+	}
+
+	@Test
+	public void testLexerThrowsExceptionAndProvidesPositionInfoWindows() {
+		final BCompoundException b = Assert.assertThrows(BCompoundException.class, () -> Helpers.parseFile("exceptions/IfAndPredicates_win.mch"));
 		Helpers.assertParseErrorLocation(b, 18, 18, 18, 18);
 	}
 }
