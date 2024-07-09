59dccde9e0a2559aa02888c1008fac8588995172
Miles Vella <673-vella@users.noreply.gitlab.cs.uni-duesseldorf.de>
2024-07-09 14:23:06 +0200

    add static helpers to pretty printer

--- bparser/src/main/java/de/be4/classicalb/core/parser/util/PrettyPrinter.java
+++ bparser/src/main/java/de/be4/classicalb/core/parser/util/PrettyPrinter.java
@@ -2,6 +2,8 @@ package de.be4.classicalb.core.parser.util;
 
 import java.io.StringWriter;
 
+import de.be4.classicalb.core.parser.node.Node;
+
 public final class PrettyPrinter extends BasePrettyPrinter {
 
 	public PrettyPrinter() {
@@ -11,5 +13,18 @@ public final class PrettyPrinter extends BasePrettyPrinter {
 	public String getPrettyPrint() {
 		this.flush();
 		return this.getWriter().toString();
+	}
+
+	public static String getPrettyPrint(Node node) {
+		PrettyPrinter pp = new PrettyPrinter();
+		node.apply(pp);
+		return pp.getPrettyPrint();
+	}
+
+	public static String getIndentedPrettyPrint(Node node) {
+		PrettyPrinter pp = new PrettyPrinter();
+		pp.setUseIndentation(true);
+		node.apply(pp);
+		return pp.getPrettyPrint();
 	}
 }
