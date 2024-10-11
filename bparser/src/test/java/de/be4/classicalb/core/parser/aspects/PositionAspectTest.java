3cf3dcf011f1758d1705453ce823785b3abadb10
dgelessus <dgelessus@users.noreply.github.com>
2024-10-11 19:32:45 +0200

    Remove trivial PositionAspectTest cases
    These things are already checked by the compiler.

--- bparser/src/test/java/de/be4/classicalb/core/parser/aspects/PositionAspectTest.java
+++ bparser/src/test/java/de/be4/classicalb/core/parser/aspects/PositionAspectTest.java
@@ -1,40 +1,18 @@
 package de.be4.classicalb.core.parser.aspects;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
-
 import de.be4.classicalb.core.parser.BParser;
 import de.be4.classicalb.core.parser.node.AAddExpression;
 import de.be4.classicalb.core.parser.node.AExpressionParseUnit;
 import de.be4.classicalb.core.parser.node.PExpression;
 import de.be4.classicalb.core.parser.node.Start;
-import de.hhu.stups.sablecc.patch.PositionedNode;
 import de.hhu.stups.sablecc.patch.SourcePosition;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 public class PositionAspectTest {
-
-	@Test
-	public void testNodeSubclassOfPositionedNode() throws Exception {
-		final String testMachine = "#EXPRESSION 1+2";
-		final BParser parser = new BParser("testcase");
-		final Start startNode = parser.parseMachine(testMachine);
-
-		assertTrue(startNode instanceof PositionedNode);
-	}
-
-	@Test
-	public void testHasGetMethods() throws Exception {
-		final String testMachine = "#EXPRESSION 1+2";
-		final BParser parser = new BParser("testcase");
-		final Start startNode = parser.parseMachine(testMachine);
-
-		startNode.getStartPos();
-		startNode.getEndPos();
-	}
-
 	@Test
 	public void testSimpleNode() throws Exception {
 		final String testMachine = "#EXPRESSION x";
