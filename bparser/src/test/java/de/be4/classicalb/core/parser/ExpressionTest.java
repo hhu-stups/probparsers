b5f2f575ae29cd255a1233f1c572c9cba9ab3245
dgelessus <dgelessus@users.noreply.github.com>
2024-10-11 19:27:48 +0200

    Replace almost all remaining parsing kind prefixes in tests

--- bparser/src/test/java/de/be4/classicalb/core/parser/ExpressionTest.java
+++ bparser/src/test/java/de/be4/classicalb/core/parser/ExpressionTest.java
@@ -13,8 +13,8 @@ import static org.junit.Assert.assertThrows;
 public class ExpressionTest {
 	@Test
 	public void testPower1() throws BCompoundException {
-		final String testMachine = "#EXPRESSION 2**3**4";
-		final String result = Helpers.getMachineAsPrologTerm(testMachine);
+		String testExpression = "2**3**4";
+		String result = Helpers.getExpressionAsPrologTerm(testExpression);
 
 		assertEquals(
 				"machine(power_of(none,integer(none,2),power_of(none,integer(none,3),integer(none,4)))).",
@@ -23,8 +23,8 @@ public class ExpressionTest {
 
 	@Test
 	public void testPower2() throws BCompoundException {
-		final String testMachine = "#EXPRESSION 2**3~";
-		final String result = Helpers.getMachineAsPrologTerm(testMachine);
+		String testExpression = "2**3~";
+		String result = Helpers.getExpressionAsPrologTerm(testExpression);
 
 		assertEquals(
 				"machine(power_of(none,integer(none,2),reverse(none,integer(none,3)))).",
@@ -33,8 +33,8 @@ public class ExpressionTest {
 
 	@Test
 	public void testPred1() throws BCompoundException {
-		final String testMachine = "#EXPRESSION pred(x)";
-		final String result = Helpers.getMachineAsPrologTerm(testMachine);
+		String testExpression = "pred(x)";
+		String result = Helpers.getExpressionAsPrologTerm(testExpression);
 
 		assertEquals(
 				"machine(function(none,predecessor(none),[identifier(none,x)])).",
@@ -43,16 +43,16 @@ public class ExpressionTest {
 
 	@Test
 	public void testPred2() throws BCompoundException {
-		final String testMachine = "#EXPRESSION pred";
-		final String result = Helpers.getMachineAsPrologTerm(testMachine);
+		String testExpression = "pred";
+		String result = Helpers.getExpressionAsPrologTerm(testExpression);
 
 		assertEquals("machine(predecessor(none)).", result);
 	}
 
 	@Test
 	public void testSucc1() throws BCompoundException {
-		final String testMachine = "#EXPRESSION succ(x)";
-		final String result = Helpers.getMachineAsPrologTerm(testMachine);
+		String testExpression = "succ(x)";
+		String result = Helpers.getExpressionAsPrologTerm(testExpression);
 
 		assertEquals(
 				"machine(function(none,successor(none),[identifier(none,x)])).",
@@ -61,16 +61,16 @@ public class ExpressionTest {
 
 	@Test
 	public void testSucc2() throws BCompoundException {
-		final String testMachine = "#EXPRESSION succ";
-		final String result = Helpers.getMachineAsPrologTerm(testMachine);
+		String testExpression = "succ";
+		String result = Helpers.getExpressionAsPrologTerm(testExpression);
 
 		assertEquals("machine(successor(none)).", result);
 	}
 
 	@Test
 	public void testAddExpression() throws BCompoundException {
-		final String testMachine = "#EXPRESSION xx.yy + 5";
-		final String result = Helpers.getMachineAsPrologTerm(testMachine);
+		String testExpression = "xx.yy + 5";
+		String result = Helpers.getExpressionAsPrologTerm(testExpression);
 
 		assertEquals(
 				"machine(add(none,identifier(none,'xx.yy'),integer(none,5))).",
@@ -79,8 +79,8 @@ public class ExpressionTest {
 
 	@Test
 	public void testSubExpression() throws BCompoundException {
-		final String testMachine = "#EXPRESSION 3 - 5";
-		final String result = Helpers.getMachineAsPrologTerm(testMachine);
+		String testExpression = "3 - 5";
+		String result = Helpers.getExpressionAsPrologTerm(testExpression);
 
 		assertEquals(
 				"machine(minus_or_set_subtract(none,integer(none,3),integer(none,5))).",
@@ -89,8 +89,8 @@ public class ExpressionTest {
 
 	@Test
 	public void testCoupleExpression() throws BCompoundException {
-		final String testMachine = "#EXPRESSION (1, aa)";
-		final String result = Helpers.getMachineAsPrologTerm(testMachine);
+		String testExpression = "(1, aa)";
+		String result = Helpers.getExpressionAsPrologTerm(testExpression);
 
 		assertEquals(
 				"machine(couple(none,[integer(none,1),identifier(none,aa)])).",
@@ -99,8 +99,8 @@ public class ExpressionTest {
 
 	@Test
 	public void testCoupleExpression2() throws BCompoundException {
-		final String testMachine = "#EXPRESSION (1, aa, bb)";
-		final String result = Helpers.getMachineAsPrologTerm(testMachine);
+		String testExpression = "(1, aa, bb)";
+		String result = Helpers.getExpressionAsPrologTerm(testExpression);
 
 		assertEquals(
 				"machine(couple(none,[integer(none,1),identifier(none,aa),identifier(none,bb)])).",
@@ -109,15 +109,14 @@ public class ExpressionTest {
 
 	@Test
 	public void testQuantifiedUnionExpression() {
-		final String testMachine = "#EXPRESSION UNION x.y.(x=0 | x )";
-		assertThrows(BCompoundException.class, () -> Helpers.getMachineAsPrologTerm(testMachine));
+		String testExpression = "UNION x.y.(x=0 | x )";
+		assertThrows(BCompoundException.class, () -> Helpers.getExpressionAsPrologTerm(testExpression));
 	}
 
 	@Test
 	public void testQuantifiedUnionExpression2() throws BCompoundException {
-
-		final String testMachine = "#EXPRESSION UNION x,y . (x=0 & y=x | (x,y) )";
-		final String result = Helpers.getMachineAsPrologTerm(testMachine);
+		String testExpression = "UNION x,y . (x=0 & y=x | (x,y) )";
+		String result = Helpers.getExpressionAsPrologTerm(testExpression);
 		assertEquals(
 				"machine(quantified_union(none,[identifier(none,x),identifier(none,y)],conjunct(none,[equal(none,identifier(none,x),integer(none,0)),equal(none,identifier(none,y),identifier(none,x))]),couple(none,[identifier(none,x),identifier(none,y)]))).",
 				result);
@@ -125,8 +124,8 @@ public class ExpressionTest {
 
 	@Test
 	public void testLambdaExpression() throws BCompoundException {
-		final String testMachine = "#EXPRESSION % x . (x=0 | x )";
-		final String result = Helpers.getMachineAsPrologTerm(testMachine);
+		String testExpression = "% x . (x=0 | x )";
+		String result = Helpers.getExpressionAsPrologTerm(testExpression);
 		assertEquals(
 				"machine(lambda(none,[identifier(none,x)],equal(none,identifier(none,x),integer(none,0)),identifier(none,x))).",
 				result);
@@ -134,14 +133,14 @@ public class ExpressionTest {
 
 	@Test(expected = BCompoundException.class)
 	public void testLambdaExpression2() throws BCompoundException {
-		final String testMachine = "#EXPRESSION % x.y.z.(x.y.z=0 | x.y.z )";
-		Helpers.getMachineAsPrologTerm(testMachine);
+		String testExpression = "% x.y.z.(x.y.z=0 | x.y.z )";
+		Helpers.getExpressionAsPrologTerm(testExpression);
 	}
 
 	@Test
 	public void testTotalRelationExpression() throws BCompoundException {
-		final String testMachine = "#EXPRESSION A <<-> B";
-		final String result = Helpers.getMachineAsPrologTerm(testMachine);
+		String testExpression = "A <<-> B";
+		String result = Helpers.getExpressionAsPrologTerm(testExpression);
 
 		assertEquals(
 				"machine(total_relation(none,identifier(none,'A'),identifier(none,'B'))).",
@@ -150,8 +149,8 @@ public class ExpressionTest {
 
 	@Test
 	public void testSurjectionRelationExpression() throws BCompoundException {
-		final String testMachine = "#EXPRESSION A <->> B";
-		final String result = Helpers.getMachineAsPrologTerm(testMachine);
+		String testExpression = "A <->> B";
+		String result = Helpers.getExpressionAsPrologTerm(testExpression);
 
 		assertEquals(
 				"machine(surjection_relation(none,identifier(none,'A'),identifier(none,'B'))).",
@@ -160,8 +159,8 @@ public class ExpressionTest {
 
 	@Test
 	public void testTotalSurjectionRelationExpression() throws BCompoundException {
-		final String testMachine = "#EXPRESSION A <<->> B";
-		final String result = Helpers.getMachineAsPrologTerm(testMachine);
+		String testExpression = "A <<->> B";
+		String result = Helpers.getExpressionAsPrologTerm(testExpression);
 
 		assertEquals(
 				"machine(total_surjection_relation(none,identifier(none,'A'),identifier(none,'B'))).",
@@ -170,8 +169,8 @@ public class ExpressionTest {
 
 	@Test
 	public void testFunction1() throws BCompoundException {
-		final String testMachine = "#EXPRESSION queues(co)";
-		final String result = Helpers.getMachineAsPrologTerm(testMachine);
+		String testExpression = "queues(co)";
+		String result = Helpers.getExpressionAsPrologTerm(testExpression);
 
 		assertEquals(
 				"machine(function(none,identifier(none,queues),[identifier(none,co)])).",
@@ -180,8 +179,8 @@ public class ExpressionTest {
 
 	@Test
 	public void testFunction2() throws BCompoundException {
-		final String testMachine = "#EXPRESSION (queues(co))(ii)";
-		final String result = Helpers.getMachineAsPrologTerm(testMachine);
+		String testExpression = "(queues(co))(ii)";
+		String result = Helpers.getExpressionAsPrologTerm(testExpression);
 
 		assertEquals(
 				"machine(function(none,function(none,identifier(none,queues),[identifier(none,co)]),[identifier(none,ii)])).",
@@ -190,16 +189,16 @@ public class ExpressionTest {
 
 	@Test
 	public void testString1() throws BCompoundException {
-		final String testMachine = "#EXPRESSION \"Hello World\"";
-		final String result = Helpers.getMachineAsPrologTerm(testMachine);
+		String testExpression = "\"Hello World\"";
+		String result = Helpers.getExpressionAsPrologTerm(testExpression);
 
 		assertEquals("machine(string(none,'Hello World')).", result);
 	}
 
 	@Test
 	public void testString2() throws BCompoundException {
-		final String testMachine = "#SUBSTITUTION text:=\"Hello World\"";
-		final String result = Helpers.getMachineAsPrologTerm(testMachine);
+		String testSubstitution = "text:=\"Hello World\"";
+		String result = Helpers.getSubstitutionAsPrologTerm(testSubstitution);
 
 		assertEquals(
 				"machine(assign(none,[identifier(none,text)],[string(none,'Hello World')])).",
@@ -208,20 +207,20 @@ public class ExpressionTest {
 
 	@Test
 	public void testEmptySequence1() throws BCompoundException {
-		final String testMachine = "#EXPRESSION [ ]";
-		final String result = Helpers.getMachineAsPrologTerm(testMachine);
+		String testExpression = "[ ]";
+		String result = Helpers.getExpressionAsPrologTerm(testExpression);
 
 		assertEquals("machine(empty_sequence(none)).", result);
 	}
 
 	@Test
 	public void testEmptySequence2() throws BCompoundException {
-		final String testMachine1 = "#EXPRESSION < >";
-		final String testMachine2 = "#EXPRESSION <>";
-		final String testMachine3 = "#EXPRESSION []";
-		final String result1 = Helpers.getMachineAsPrologTerm(testMachine1);
-		final String result2 = Helpers.getMachineAsPrologTerm(testMachine2);
-		final String result3 = Helpers.getMachineAsPrologTerm(testMachine3);
+		String testMachine1 = "< >";
+		String testMachine2 = "<>";
+		String testMachine3 = "[]";
+		String result1 = Helpers.getExpressionAsPrologTerm(testMachine1);
+		String result2 = Helpers.getExpressionAsPrologTerm(testMachine2);
+		String result3 = Helpers.getExpressionAsPrologTerm(testMachine3);
 
 		assertEquals("machine(empty_sequence(none)).", result1);
 		assertEquals(result1, result2);
@@ -231,8 +230,8 @@ public class ExpressionTest {
 
 	@Test
 	public void testImage1() throws BCompoundException {
-		final String testMachine = "#EXPRESSION sex~[{woman}] - dom(husband)";
-		final String result = Helpers.getMachineAsPrologTerm(testMachine);
+		String testExpression = "sex~[{woman}] - dom(husband)";
+		String result = Helpers.getExpressionAsPrologTerm(testExpression);
 
 		assertEquals(
 				"machine(minus_or_set_subtract(none,image(none,reverse(none,identifier(none,sex)),set_extension(none,[identifier(none,woman)])),domain(none,identifier(none,husband)))).",
@@ -241,8 +240,8 @@ public class ExpressionTest {
 
 	@Test
 	public void testImage2() throws BCompoundException {
-		final String testMachine = "#EXPRESSION {a |-> b}[{a}]";
-		final String result = Helpers.getMachineAsPrologTerm(testMachine);
+		String testExpression = "{a |-> b}[{a}]";
+		String result = Helpers.getExpressionAsPrologTerm(testExpression);
 
 		assertEquals(
 				"machine(image(none,set_extension(none,[couple(none,[identifier(none,a),identifier(none,b)])]),set_extension(none,[identifier(none,a)]))).",
@@ -251,8 +250,8 @@ public class ExpressionTest {
 
 	@Test
 	public void testImage3() throws BCompoundException {
-		final String testMachine = "#EXPRESSION {a |-> b, c |-> d} - {c |-> d}[{a}] - {b}";
-		final String result = Helpers.getMachineAsPrologTerm(testMachine);
+		String testExpression = "{a |-> b, c |-> d} - {c |-> d}[{a}] - {b}";
+		String result = Helpers.getExpressionAsPrologTerm(testExpression);
 
 		assertEquals(
 				"machine(minus_or_set_subtract(none,minus_or_set_subtract(none,set_extension(none,[couple(none,[identifier(none,a),identifier(none,b)]),couple(none,[identifier(none,c),identifier(none,d)])]),image(none,set_extension(none,[couple(none,[identifier(none,c),identifier(none,d)])]),set_extension(none,[identifier(none,a)]))),set_extension(none,[identifier(none,b)]))).",
@@ -261,8 +260,8 @@ public class ExpressionTest {
 
 	@Test
 	public void testIntervalMinus() throws BCompoundException {
-		final String testMachine = "#EXPRESSION 1..5-1";
-		final String result = Helpers.getMachineAsPrologTerm(testMachine);
+		String testExpression = "1..5-1";
+		String result = Helpers.getExpressionAsPrologTerm(testExpression);
 
 		assertEquals(
 				"machine(interval(none,integer(none,1),minus_or_set_subtract(none,integer(none,5),integer(none,1)))).",
@@ -271,8 +270,8 @@ public class ExpressionTest {
 
 	@Test
 	public void testCoupleMinus() throws BCompoundException {
-		final String testMachine = "#EXPRESSION 1 |-> 5-1";
-		final String result = Helpers.getMachineAsPrologTerm(testMachine);
+		String testExpression = "1 |-> 5-1";
+		String result = Helpers.getExpressionAsPrologTerm(testExpression);
 
 		assertEquals(
 				"machine(couple(none,[integer(none,1),minus_or_set_subtract(none,integer(none,5),integer(none,1))])).",
@@ -281,8 +280,8 @@ public class ExpressionTest {
 
 	@Test
 	public void testPlusMinus() throws BCompoundException {
-		final String testMachine = "#EXPRESSION 1 + 5 - 3";
-		final String result = Helpers.getMachineAsPrologTerm(testMachine);
+		String testExpression = "1 + 5 - 3";
+		String result = Helpers.getExpressionAsPrologTerm(testExpression);
 
 		assertEquals(
 				"machine(minus_or_set_subtract(none,add(none,integer(none,1),integer(none,5)),integer(none,3))).",
@@ -291,8 +290,8 @@ public class ExpressionTest {
 
 	@Test
 	public void testUnionMinus() throws BCompoundException {
-		final String testMachine = "#EXPRESSION s1 - {x} \\/ {y}";
-		final String result = Helpers.getMachineAsPrologTerm(testMachine);
+		String testExpression = "s1 - {x} \\/ {y}";
+		String result = Helpers.getExpressionAsPrologTerm(testExpression);
 
 		assertEquals(
 				"machine(union(none,minus_or_set_subtract(none,identifier(none,s1),set_extension(none,[identifier(none,x)])),set_extension(none,[identifier(none,y)]))).",
@@ -301,26 +300,26 @@ public class ExpressionTest {
 
 	@Test
 	public void testConcat() throws BCompoundException {
-		final String result = Helpers.getMachineAsPrologTerm("#EXPRESSION s^t");
+		String result = Helpers.getExpressionAsPrologTerm("s^t");
 		assertEquals("machine(concat(none,identifier(none,s),identifier(none,t))).", result);
 	}
 
 	@Test
 	public void testString() throws BCompoundException {
-		final String result = Helpers.getMachineAsPrologTerm("#EXPRESSION \"test\"");
+		String result = Helpers.getExpressionAsPrologTerm("\"test\"");
 		assertEquals("machine(string(none,test)).", result);
 	}
 
 	@Test
 	public void testEmptyString() throws BCompoundException {
-		final String result = Helpers.getMachineAsPrologTerm("#EXPRESSION \"\"");
+		String result = Helpers.getExpressionAsPrologTerm("\"\"");
 		assertEquals("machine(string(none,'')).", result);
 	}
 
 	@Test
 	public void testComprehensionSets() throws BCompoundException {
-		final String expected = "machine(comprehension_set(none,[identifier(none,i)],greater(none,identifier(none,i),integer(none,0)))).";
-		final String standard = Helpers.getMachineAsPrologTerm("#EXPRESSION {i|i>0}");
+		String expected = "machine(comprehension_set(none,[identifier(none,i)],greater(none,identifier(none,i),integer(none,0)))).";
+		String standard = Helpers.getExpressionAsPrologTerm("{i|i>0}");
 		assertEquals(expected, standard);
 	}
 
@@ -331,15 +330,15 @@ public class ExpressionTest {
 
 	@Test
 	public void testRelationalImagePrio() throws BCompoundException {
-		final String actual = Helpers.getMachineAsPrologTerm("#EXPRESSION c~[s]*x");
-		final String expected = "machine(mult_or_cart(none,image(none,reverse(none,identifier(none,c)),identifier(none,s)),identifier(none,x))).";
+		String actual = Helpers.getExpressionAsPrologTerm("c~[s]*x");
+		String expected = "machine(mult_or_cart(none,image(none,reverse(none,identifier(none,c)),identifier(none,s)),identifier(none,x))).";
 		assertEquals(expected, actual);
 	}
 
 	@Test
 	public void testLargeInteger() throws BCompoundException {
-		final String actual = Helpers.getMachineAsPrologTerm("#EXPRESSION 922337203685477580756");
-		final String expected = "machine(integer(none,922337203685477580756)).";
+		String actual = Helpers.getExpressionAsPrologTerm("922337203685477580756");
+		String expected = "machine(integer(none,922337203685477580756)).";
 		assertEquals(expected, actual);
 	}
 }
--- bparser/src/test/java/de/be4/classicalb/core/parser/languageextension/LetExpressionTest.java
+++ bparser/src/test/java/de/be4/classicalb/core/parser/languageextension/LetExpressionTest.java
@@ -11,8 +11,8 @@ import util.Helpers;
 public class LetExpressionTest {
 	@Test
 	public void testSingleIdentifierLetExpression() throws BCompoundException {
-		final String testMachine = "#EXPRESSION (LET x BE x = 5 IN x+1 END)";
-		final String result = Helpers.getMachineAsPrologTerm(testMachine);
+		String testExpression = "(LET x BE x = 5 IN x+1 END)";
+		String result = Helpers.getExpressionAsPrologTerm(testExpression);
 
 		assertEquals(
 				"machine(let_expression(none,[identifier(none,x)],equal(none,identifier(none,x),integer(none,5)),add(none,identifier(none,x),integer(none,1)))).",
@@ -21,8 +21,8 @@ public class LetExpressionTest {
 
 	@Test
 	public void testMultipleIdentifiersLetExpression() throws BCompoundException {
-		final String testMachine = "#EXPRESSION (LET x, y BE x = 5 & y = 7 IN x+y END)";
-		final String result = Helpers.getMachineAsPrologTerm(testMachine);
+		String testExpression = "(LET x, y BE x = 5 & y = 7 IN x+y END)";
+		String result = Helpers.getExpressionAsPrologTerm(testExpression);
 
 		assertEquals(
 				"machine(let_expression(none,[identifier(none,x),identifier(none,y)],conjunct(none,[equal(none,identifier(none,x),integer(none,5)),equal(none,identifier(none,y),integer(none,7))]),add(none,identifier(none,x),identifier(none,y)))).",
