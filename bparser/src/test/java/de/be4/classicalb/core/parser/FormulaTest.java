2cc119346247b127573b677dd09968ab45e70004
dgelessus <dgelessus@users.noreply.github.com>
2024-10-11 02:02:59 +0200

    Combine a few loose tests into FormulaTest

--- bparser/src/test/java/de/be4/classicalb/core/parser/FormulaTest.java
+++ bparser/src/test/java/de/be4/classicalb/core/parser/FormulaTest.java
@@ -4,7 +4,16 @@ import org.junit.Test;
 
 import de.be4.classicalb.core.parser.exceptions.BCompoundException;
 
+import util.Helpers;
+
+import static org.junit.Assert.assertEquals;
+
 public class FormulaTest {
+	@Test
+	public void testFormulaOutput() throws BCompoundException {
+		String output = Helpers.getFormulaAsPrologTerm("1+1");
+		assertEquals(output, "machine(add(none,integer(none,1),integer(none,1))).");
+	}
 
 	@Test
 	public void testFomulaExpression() throws Exception {
@@ -30,4 +39,16 @@ public class FormulaTest {
 		new BParser().parseFormula(formula);
 	}
 
+	// Ensure that JIRA ticket PROB-295 is fixed.
+	@Test
+	public void ticketProb295() throws BCompoundException {
+		// String input = "#x. /* comment */ (x>1000 & x<2**10)";
+		String input1 = "#x. /*buh */ (  x>1000 & x<2**10)";
+		String input2 = "#x.(/*buh */ x>1000 & x<2**10)";
+
+		String result1 = Helpers.getFormulaAsPrologTerm(input1);
+		String result2 = Helpers.getFormulaAsPrologTerm(input2);
+
+		assertEquals(result1, result2);
+	}
 }
