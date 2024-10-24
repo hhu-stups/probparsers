638b6bb23ac8c7bb95f9e8abd3182c882e30febb
dgelessus <dgelessus@users.noreply.github.com>
2024-10-24 19:12:52 +0200

    Remove old commented out code from StringLiteralNotClosedTest

--- bparser/src/test/java/de/be4/classicalb/core/parser/StringLiteralNotClosedTest.java
+++ bparser/src/test/java/de/be4/classicalb/core/parser/StringLiteralNotClosedTest.java
@@ -24,25 +24,11 @@ public class StringLiteralNotClosedTest {
 	@Test
 	public void testStringLiteralNotClosedLongString() {
 		final String testMachine = "MACHINE Test CONSTANTS the_string PROPERTIES the_string = \"not closed"
-				//+ randomString(100) // could accidentally close the string !
 				+ "'%'''%*__abcdezABCZäîüß ab 12ab ==> <===> !@#$%^&*()(]{[||]};;; "
 				+ "== DEFINITIONS a=== MACHINE <<> ~`DEFINITIONS` ''  "
 				+ "12345678999911112334234234345236245634563456345635463465345634563456345346534563546 "
 				+ "END";
 		final PreParseException e = Helpers.assertThrowsCompound(PreParseException.class, () -> Helpers.getMachineAsPrologTerm(testMachine));
-		// System.out.println("Exception = " + e.getLocalizedMessage());
 		assertTrue(e.getLocalizedMessage(),e.getLocalizedMessage().contains("Unknown token:"));
 	}
-
-// 	String randomString(final int length) {
-// 		Random r = new Random(); // perhaps make it a class variable so you
-// 									// don't make a new one every time
-// 		StringBuilder sb = new StringBuilder();
-// 		for (int i = 0; i < length; i++) {
-// 			char c = (char) (r.nextInt((int) (Character.MAX_VALUE)));
-// 			sb.append(c);
-// 		}
-// 		return sb.toString();
-// 	}
-
 }
