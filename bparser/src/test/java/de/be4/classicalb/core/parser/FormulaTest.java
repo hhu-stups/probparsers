82c14b90d53bb3d0dc569e0fbce5ce27c8d739fc
dgelessus <dgelessus@users.noreply.github.com>
2024-10-24 19:11:44 +0200

    Combine drophaskellregressions tests into appropriate general tests

--- bparser/src/test/java/de/be4/classicalb/core/parser/FormulaTest.java
+++ bparser/src/test/java/de/be4/classicalb/core/parser/FormulaTest.java
@@ -1,12 +1,14 @@
 package de.be4.classicalb.core.parser;
 
-import org.junit.Test;
-
 import de.be4.classicalb.core.parser.exceptions.BCompoundException;
+import de.be4.classicalb.core.parser.node.Start;
+
+import org.junit.Test;
 
 import util.Helpers;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class FormulaTest {
 	@Test
@@ -50,5 +52,27 @@ public class FormulaTest {
 		String result2 = Helpers.getFormulaAsPrologTerm(input2);
 
 		assertEquals(result1, result2);
+	}
+
+	@Test
+	public void test1002file1() throws BCompoundException {
+		String source = "cajas : STRING +-> NATURAL & num : STRING & m : NATURAL & m > cajas(num) & num : dom(cajas) & cajas /= { } & { num |-> ( ( cajas(num) ) - m ) } /= { } & dom({ num |-> ( ( cajas(num) ) - m ) }) <<: dom(cajas)";
+		Start result = new BParser().parseFormula(source);
+		assertNotNull(result);
+	}
+
+	@Test
+	public void test1002file2() throws BCompoundException {
+
+		String source = " clients : STRING +-> STRING & balances : STRING +-> NATURAL & owners : STRING <-> STRING & u : STRING & n : STRING & m : NATURAL & m > balances(n) & m < 100000";
+		Start result = new BParser().parseFormula(source);
+		assertNotNull(result);
+	}
+
+	@Test
+	public void test1002file3() throws BCompoundException {
+		String source = "srv : {\"HS\",\"SC\",\"COM\",\"VOM\",\"SDA\",\"RDA\",\"TD\",\"RA\",\"GMR\",\"LDM\",\"EP\",\"MSP\"} & om : {\"SAFETY\",\"NOM\",\"DIAG\"} & acquiring : {TRUE,FALSE} & prepData : {TRUE,FALSE} & prepDataType : {\"SD\",\"HD\",\"MD\"} & page : NATURAL & ia : NATURAL & fa : NATURAL & lpck : seq(STRING) & lpckDT : {\"CR\",\"CD\",\"OM\",\"ND\",\"ASD\",\"AMD\",\"MSE\",\"ICS\",\"LS\"} & csrs : {\"SD\",\"HD\",\"MD\"} --> NATURAL & totCSR : {\"SD\",\"HD\",\"MD\"} --> NATURAL & csc : NATURAL & mem : 1 .. 1024 --> STRING & modMem : POW(NATURAL) & sdwp : NATURAL & sparam : {\"HGT\",\"ILP\",\"AICST\"} --> NATURAL & time : NATURAL & processingCmd : {TRUE,FALSE} & iain : NATURAL & data : seq(STRING) & c : NATURAL & processingCmd = TRUE & srv = \"LDM\" & om = \"NOM\" & csc > 0 & c = csc - 1 & sparam(\"ILP\") <= iain & iain + card(data) : 0 .. 32 & {x | x-iain:dom(data)} /\\ modMem = { } & {x | x-iain:dom(data)} /= { } & modMem = { }";
+		Start result = new BParser().parseFormula(source);
+		assertNotNull(result);
 	}
 }
--- bparser/src/test/java/de/be4/classicalb/core/parser/definitions/DefinitionsTest.java
+++ bparser/src/test/java/de/be4/classicalb/core/parser/definitions/DefinitionsTest.java
@@ -1,7 +1,14 @@
 package de.be4.classicalb.core.parser.definitions;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import de.be4.classicalb.core.parser.BParser;
 import de.be4.classicalb.core.parser.exceptions.BCompoundException;
 import de.be4.classicalb.core.parser.exceptions.CheckException;
+import de.be4.classicalb.core.parser.exceptions.PreParseException;
+import de.be4.classicalb.core.parser.node.Start;
 
 import org.junit.Ignore;
 import org.junit.Test;
@@ -335,4 +342,16 @@ public class DefinitionsTest {
 		//TODO
 	}
 
+	@Test
+	public void testAddDefinitionsFromOtherParser() throws BCompoundException, PreParseException, URISyntaxException {
+		BParser parser = new BParser();
+		parser.parseFile(new File(this.getClass().getResource("/parsable/InfiniteParityFunction.mch").toURI()));
+
+		String code = "not(finite({x|x>2}))";
+		BParser parser2 = new BParser();
+
+		parser2.getDefinitions().addDefinitions(parser.getDefinitions());
+		Start parse = parser2.parseFormula(code);
+		assertNotNull(parse);
+	}
 }
