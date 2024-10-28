hhu-stups/probparsers

commit 0790d10541546430f9f67058dd0f8b12b268ecd2

Refactor PrettyPrinter to support indented printing

bparser/src/main/java/de/be4/classicalb/core/parser/analysis/transforming/SyntaxExtensionTranslator.java
@@ -9,6 +9,7 @@ import de.be4.classicalb.core.parser.node.*;
 import de.be4.classicalb.core.parser.util.Utils;
 
 public class SyntaxExtensionTranslator extends OptimizedTraversingAdapter {
+
 	private static String cleanDescriptionText(final String descriptionText) {
 		String formatted = descriptionText;
 		if (descriptionText.endsWith("*/")) {
@@ -27,17 +28,15 @@ public class SyntaxExtensionTranslator extends OptimizedTraversingAdapter {
 	}
 
 	private static void checkArgumentCount(final AFunctionExpression node, final int count) {
-		if (node.getParameters().size() != count) {
+		int paramCount = node.getParameters().size();
+		if (paramCount != count) {
 			final String funcName = Utils.getAIdentifierAsString((AIdentifierExpression) node.getIdentifier());
-			throw new VisitorException(new CheckException("Built-in function " + funcName + " expects exactly " + count + " arguments, but got " + node.getParameters().size(), node));
+			throw new VisitorException(new CheckException("Built-in function " + funcName + " expects exactly " + count + " argument(s), but got " + paramCount, node));
 		}
 	}
 
 	private static PExpression checkSingleArgument(final AFunctionExpression node) {
-		if (node.getParameters().size() != 1) {
-			final String funcName = Utils.getAIdentifierAsString((AIdentifierExpression) node.getIdentifier());
-			throw new VisitorException(new CheckException("Built-in function " + funcName + " expects exactly one argument, but got " + node.getParameters().size(), node));
-		}
+		checkArgumentCount(node, 1);
 		return node.getParameters().get(0);
 	}
 
@@ -55,7 +54,7 @@ public class SyntaxExtensionTranslator extends OptimizedTraversingAdapter {
 	}
 
 	@Override
-	public void caseAMultilineStringExpression(AMultilineStringExpression node) {
+	public void outAMultilineStringExpression(AMultilineStringExpression node) {
 		final TMultilineStringContent content = node.getContent();
 		final String text = content.getText();
 		// multiline strings do not have surrounding "
@@ -73,7 +72,7 @@ public class SyntaxExtensionTranslator extends OptimizedTraversingAdapter {
 		// and process backslash escape sequences.
 		final String text = node.getText();
 		final String unescaped = Utils.unescapeStringContents(Utils.removeSurroundingQuotes(text, '"'));
-		node.replaceBy(new TStringLiteral(unescaped, node.getLine(), node.getPos()));
+		node.setText(unescaped);
 	}
 
 	@Override
@@ -86,17 +85,17 @@ public class SyntaxExtensionTranslator extends OptimizedTraversingAdapter {
 				throw new VisitorException(new CheckException("A quoted identifier cannot contain a dot. Please quote only the identifiers before and after the dot, but not the dot itself, e. g.: " + fixed, node));
 			}
 			final String unescapedText = Utils.unescapeStringContents(Utils.removeSurroundingQuotes(text, '`'));
-			node.replaceBy(new TIdentifierLiteral(unescapedText, node.getLine(), node.getPos()));
+			node.setText(unescapedText);
 		}
 	}
 
 	@Override
-	public void caseAHexIntegerExpression(AHexIntegerExpression node) {
+	public void outAHexIntegerExpression(AHexIntegerExpression node) {
 		// transform hex_integer into integer case (so that Prolog AST does not have to deal with new node):
 		THexLiteral literal = node.getLiteral();
 		String text = literal.getText().substring(2);
-		//int value = Integer.valueOf(text, 16);
 		BigInteger value = new BigInteger(text, 16);
+
 		// generate an integer literal:
 		TIntegerLiteral tIntLiteral = new TIntegerLiteral(value.toString(), literal.getLine(), literal.getPos());
 		AIntegerExpression intNode = new AIntegerExpression(tIntLiteral);
@@ -131,15 +130,14 @@ public class SyntaxExtensionTranslator extends OptimizedTraversingAdapter {
 	 * @param node the function expression to transform
 	 */
 	@Override
-	public void caseAFunctionExpression(final AFunctionExpression node) {
+	public void outAFunctionExpression(final AFunctionExpression node) {
 		if (!(node.getIdentifier() instanceof AIdentifierExpression)) {
-			super.caseAFunctionExpression(node);
 			return;
 		}
+
 		final String funcName = Utils.getAIdentifierAsString((AIdentifierExpression) node.getIdentifier());
 		if (Utils.isQuoted(funcName, '`')) {
 			// Allow suppressing the built-in function by backquoting the function identifier.
-			super.caseAFunctionExpression(node);
 			return;
 		}
 
@@ -208,12 +206,13 @@ public class SyntaxExtensionTranslator extends OptimizedTraversingAdapter {
 				break;
 
 			case "bin":
-				if (node.getParameters().size() == 1) {
+				int paramCount = node.getParameters().size();
+				if (paramCount == 1) {
 					replacement = new ABinExpression(node.getParameters().get(0), null, null);
-				} else if (node.getParameters().size() == 3) {
+				} else if (paramCount == 3) {
 					replacement = new ABinExpression(node.getParameters().get(0), node.getParameters().get(1), node.getParameters().get(2));
 				} else {
-					throw new VisitorException(new CheckException("Built-in function " + funcName + " expects 1 or 3 arguments, but got " + node.getParameters().size(), node));
+					throw new VisitorException(new CheckException("Built-in function " + funcName + " expects 1 or 3 arguments, but got " + paramCount, node));
 				}
 				break;
 
@@ -230,13 +229,11 @@ public class SyntaxExtensionTranslator extends OptimizedTraversingAdapter {
 				break;
 
 			default:
-				super.caseAFunctionExpression(node);
 				return;
 		}
 
-		node.replaceBy(replacement);
 		replacement.setStartPos(node.getStartPos());
 		replacement.setEndPos(node.getEndPos());
-		replacement.apply(this);
+		node.replaceBy(replacement);
 	}
 }

bparser/src/test/java/de/be4/classicalb/core/parser/prettyprinter/PrettyMachinePrinterTest.java
@@ -1,13 +1,15 @@
 package de.be4.classicalb.core.parser.prettyprinter;
 
-import de.be4.classicalb.core.parser.util.Utils;
-import org.junit.Test;
-import util.Helpers;
-
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+
+import de.be4.classicalb.core.parser.util.Utils;
+
+import org.junit.Test;
+
+import util.Helpers;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -17,25 +19,47 @@ public class PrettyMachinePrinterTest {
 	@Test
 	public void testPrettyPrint() {
 		final String testMachine = "MACHINE Test\n" +
-				"VARIABLES x, y\n" +
-				"INVARIANT x:INTEGER & y:INTEGER\n" +
-				"INITIALISATION x := 1 || y := 2\n" +
-				"OPERATIONS\n" +
-				"foo = skip;\n" +
-				"bar = skip\n" +
-				"END";
+			"VARIABLES x, y\n" +
+			"INVARIANT x:INTEGER & y:INTEGER\n" +
+			"INITIALISATION x := 1 || y := 2\n" +
+			"OPERATIONS\n" +
+			"foo = skip;\n" +
+			"bar = skip\n" +
+			"END";
 		final String result1 = Helpers.getPrettyPrint(testMachine);
 
 		assertFalse(result1.isEmpty());
+		assertEquals(testMachine, result1);
+	}
 
+	@Test
+	public void testPrettyPrint_Indent() {
+		final String testMachine = "MACHINE Test\n" +
+			"    VARIABLES\n" +
+			"        x,\n" +
+			"        y\n" +
+			"    INVARIANT\n" +
+			"        x:INTEGER & y:INTEGER\n" +
+			"    INITIALISATION\n" +
+			"        x := 1\n" +
+			"        || y := 2\n" +
+			"    OPERATIONS\n" +
+			"        foo =\n" +
+			"            skip;\n" +
+			"        bar =\n" +
+			"            skip\n" +
+			"END";
+		final String result1 = Helpers.getPrettyPrintWithIndentation(testMachine);
+
+		assertFalse(result1.isEmpty());
 		assertEquals(testMachine, result1);
 	}
 
 	@Test
 	public void testPrettyPrint2() throws IOException, URISyntaxException {
 		final URI uri = this.getClass()
-				.getResource("/prettyprinter/PrettyPrinter.mch")
-				.toURI();
+			.getResource("/prettyprinter/PrettyPrinter.mch")
+			.toURI();
 		final File file = new File(uri);
 		final String testMachine = Utils.readFile(file);
 		final String result1 = Helpers.getPrettyPrint(testMachine);
@@ -47,14 +71,44 @@ public class PrettyMachinePrinterTest {
 	}
 
 	@Test
+	public void testPrettyPrint2_Indent() throws IOException, URISyntaxException {
+		final URI uri = this.getClass()
+			.getResource("/prettyprinter/PrettyPrinter.mch")
+			.toURI();
+		final File file = new File(uri);
+		final String testMachine = Utils.readFile(file);
+		final String result1 = Helpers.getPrettyPrintWithIndentation(testMachine);
+		final String result2 = Helpers.getPrettyPrintWithIndentation(result1);
+
+		assertFalse(result1.isEmpty());
+		assertFalse(result2.isEmpty());
+		assertEquals(result1, result2);
+	}
+
+	@Test
 	public void testPrettyPrint3() throws IOException, URISyntaxException {
 		final URI uri = this.getClass()
-				.getResource("/prettyprinter/PrettyPrinter2.mch")
-				.toURI();
+			.getResource("/prettyprinter/PrettyPrinter2.mch")
+			.toURI();
 		final File file = new File(uri);
 		final String testMachine = Utils.readFile(file);
 		final String result1 = Helpers.getPrettyPrint(testMachine);
 		final String result2 = Helpers.getPrettyPrint(result1);
+
+		assertFalse(result1.isEmpty());
+		assertFalse(result2.isEmpty());
+		assertEquals(result1, result2);
+	}
+
+	@Test
+	public void testPrettyPrint3_Indent() throws IOException, URISyntaxException {
+		final URI uri = this.getClass()
+			.getResource("/prettyprinter/PrettyPrinter2.mch")
+			.toURI();
+		final File file = new File(uri);
+		final String testMachine = Utils.readFile(file);
+		final String result1 = Helpers.getPrettyPrintWithIndentation(testMachine);
+		final String result2 = Helpers.getPrettyPrintWithIndentation(result1);
 
 		assertFalse(result1.isEmpty());
 		assertFalse(result2.isEmpty());

bparser/src/test/java/de/be4/classicalb/core/parser/prettyprinter/PrettyPrinterRenamingInvalidTest.java
@@ -36,7 +36,7 @@ public final class PrettyPrinterRenamingInvalidTest {
 		Start ast = new BParser().parseExpression(this.expr);
 		PrettyPrinter pp = new PrettyPrinter();
 		ast.apply(pp);
-		
+
 		Start ast2 = new BParser().parseExpression(pp.getPrettyPrint());
 		PrettyPrinter pp2 = new PrettyPrinter();
 		ast2.apply(pp2);
@@ -44,7 +44,7 @@ public final class PrettyPrinterRenamingInvalidTest {
 		Assert.assertEquals(Helpers.getTreeAsPrologTerm(ast), Helpers.getTreeAsPrologTerm(ast2));
 		Assert.assertEquals(pp.getPrettyPrint(), pp2.getPrettyPrint());
 	}
-	
+
 	// Suffix and literal renaming cannot handle such invalid identifiers
 	// and we don't care about their precise behavior in this case.
 }

bparser/src/test/java/de/be4/classicalb/core/parser/prettyprinter/PrettyPrinterRenamingKeywordsTest.java
@@ -51,7 +51,7 @@ public final class PrettyPrinterRenamingKeywordsTest {
 		Start ast = new BParser().parseExpression(this.expr);
 		PrettyPrinter pp = new PrettyPrinter();
 		ast.apply(pp);
-		
+
 		Start ast2 = new BParser().parseExpression(pp.getPrettyPrint());
 		PrettyPrinter pp2 = new PrettyPrinter();
 		ast2.apply(pp2);
@@ -67,7 +67,7 @@ public final class PrettyPrinterRenamingKeywordsTest {
 		pp.setRenaming(new SuffixIdentifierRenaming());
 		ast.apply(pp);
 		Assert.assertEquals(this.suffixedPP, pp.getPrettyPrint());
-		
+
 		Start ast2 = new BParser().parseExpression(pp.getPrettyPrint());
 		PrettyPrinter pp2 = new PrettyPrinter();
 		pp2.setRenaming(new SuffixIdentifierRenaming());

bparser/src/test/java/de/be4/classicalb/core/parser/prettyprinter/PrettyPrinterRenamingPlainTest.java
@@ -50,7 +50,7 @@ public final class PrettyPrinterRenamingPlainTest {
 		Start ast = new BParser().parseExpression(this.expr);
 		PrettyPrinter pp = new PrettyPrinter();
 		ast.apply(pp);
-		
+
 		Start ast2 = new BParser().parseExpression(pp.getPrettyPrint());
 		PrettyPrinter pp2 = new PrettyPrinter();
 		ast2.apply(pp2);
@@ -65,7 +65,7 @@ public final class PrettyPrinterRenamingPlainTest {
 		PrettyPrinter pp = new PrettyPrinter();
 		pp.setRenaming(new SuffixIdentifierRenaming());
 		ast.apply(pp);
-		
+
 		Start ast2 = new BParser().parseExpression(pp.getPrettyPrint());
 		PrettyPrinter pp2 = new PrettyPrinter();
 		pp2.setRenaming(new SuffixIdentifierRenaming());
@@ -81,7 +81,7 @@ public final class PrettyPrinterRenamingPlainTest {
 		PrettyPrinter pp = new PrettyPrinter();
 		pp.setRenaming(IIdentifierRenaming.LITERAL);
 		ast.apply(pp);
-		
+
 		Start ast2 = new BParser().parseExpression(pp.getPrettyPrint());
 		PrettyPrinter pp2 = new PrettyPrinter();
 		pp2.setRenaming(IIdentifierRenaming.LITERAL);

bparser/src/test/java/util/Helpers.java
@@ -46,6 +46,20 @@ public class Helpers {
 		return pp.getPrettyPrint();
 	}
 
+	public static String getPrettyPrintWithIndentation(final String testMachine) {
+		final BParser parser = new BParser("testcase");
+		Start startNode;
+		try {
+			startNode = parser.parseMachine(testMachine);
+		} catch (BCompoundException e) {
+			throw new RuntimeException(e);
+		}
+		PrettyPrinter pp = new PrettyPrinter();
+		pp.setUseIndentation(true);
+		startNode.apply(pp);
+		return pp.getPrettyPrint();
+	}
+
 	/**
 	 * Clean up line separators in a Prolog term string,
 	 * so that it can be compared more easily using {@link Assert#assertEquals(Object, Object)} and similar methods.

commit df077524e0c5a8a6825b4210ced57f2609f3717d

Fix ability to suppress built-in functions using backquotes
This was accidentally broken in 0790d10541546430f9f67058dd0f8b12b268ecd2
and nobody noticed, because we didn't have any tests for this case.

bparser/src/main/java/de/be4/classicalb/core/parser/analysis/transforming/SyntaxExtensionTranslator.java
@@ -172,14 +172,19 @@ public class SyntaxExtensionTranslator extends OptimizedTraversingAdapter {
 	 * @param node the function expression to transform
 	 */
 	@Override
-	public void outAFunctionExpression(final AFunctionExpression node) {
+	public void caseAFunctionExpression(AFunctionExpression node) {
+		// It is important that this transformation runs *before* its children are processed
+		// so that backquoted identifiers haven't been unquoted yet.
+		// Otherwise the special case for backquoted identifiers below will not work correctly.
 		if (!(node.getIdentifier() instanceof AIdentifierExpression)) {
+			super.caseAFunctionExpression(node);
 			return;
 		}
 
 		final String funcName = Utils.getAIdentifierAsString((AIdentifierExpression) node.getIdentifier());
 		if (Utils.isQuoted(funcName, '`')) {
 			// Allow suppressing the built-in function by backquoting the function identifier.
+			super.caseAFunctionExpression(node);
 			return;
 		}
 
@@ -271,11 +276,13 @@ public class SyntaxExtensionTranslator extends OptimizedTraversingAdapter {
 				break;
 
 			default:
+				super.caseAFunctionExpression(node);
 				return;
 		}
 
 		replacement.setStartPos(node.getStartPos());
 		replacement.setEndPos(node.getEndPos());
 		node.replaceBy(replacement);
+		replacement.apply(this);
 	}
 }

bparser/src/test/java/de/be4/classicalb/core/parser/languageextension/SyntaxExtensionTest.java
@@ -91,4 +91,32 @@ public class SyntaxExtensionTest {
 				result);
 	}
 
+	@Test
+	public void testTreeKeywords() throws BCompoundException {
+		assertEquals(
+			"machine(abstract_machine(none,machine(none),machine_header(none,'TreeKeywords',[]),[constants(none,[identifier(none,t)]),properties(none,conjunct(none,[member(none,identifier(none,t),btree(none,integer_set(none))),equal(none,identifier(none,t),bin(none,bin(none,integer(none,1)),integer(none,0),bin(none,integer(none,2)))),equal(none,top(none,identifier(none,t)),integer(none,0)),equal(none,top(none,left(none,identifier(none,t))),integer(none,1)),equal(none,top(none,right(none,identifier(none,t))),integer(none,2))]))])).",
+			Helpers.getMachineAsPrologTerm("MACHINE TreeKeywords CONSTANTS t PROPERTIES t : btree(INTEGER) & t = bin(bin(1), 0, bin(2)) & top(t) = 0 & top(left(t)) = 1 & top(right(t)) = 2 END")
+		);
+	}
+
+	@Test
+	public void testTreeKeywordsAsRegularIdentifiers() throws BCompoundException {
+		String expected = "machine(abstract_machine(none,machine(none),machine_header(none,'TreeKeywords',[]),[sets(none,[enumerated_set(none,'DIRECTION',[identifier(none,top),identifier(none,left),identifier(none,right)])]),constants(none,[identifier(none,bin)]),properties(none,equal(none,identifier(none,bin),set_extension(none,[identifier(none,left),identifier(none,right)])))])).";
+		assertEquals(
+			expected,
+			Helpers.getMachineAsPrologTerm("MACHINE TreeKeywords SETS DIRECTION = {top, left, right} CONSTANTS bin PROPERTIES bin = {left, right} END")
+		);
+		assertEquals(
+			expected,
+			Helpers.getMachineAsPrologTerm("MACHINE TreeKeywords SETS DIRECTION = {`top`, `left`, `right`} CONSTANTS `bin` PROPERTIES `bin` = {`left`, `right`} END")
+		);
+	}
+
+	@Test
+	public void testTreeKeywordsAsFunctionIdentifiers() throws BCompoundException {
+		assertEquals(
+			"machine(abstract_machine(none,machine(none),machine_header(none,'TreeKeywords',[]),[constants(none,[identifier(none,bin),identifier(none,top)]),properties(none,conjunct(none,[equal(none,identifier(none,bin),set_extension(none,[couple(none,[boolean_false(none),integer(none,0)]),couple(none,[boolean_true(none),integer(none,1)])])),equal(none,identifier(none,top),set_extension(none,[couple(none,[integer(none,0),integer(none,1)]),couple(none,[integer(none,1),integer(none,1)])])),equal(none,function(none,identifier(none,bin),[boolean_true(none)]),function(none,identifier(none,top),[integer(none,1)]))]))])).",
+			Helpers.getMachineAsPrologTerm("MACHINE TreeKeywords CONSTANTS bin, top PROPERTIES bin = {FALSE|->0, TRUE|->1} & top = {0|->1, 1|->1} & `bin`(TRUE) = `top`(1) END")
+		);
+	}
 }