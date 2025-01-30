230df3541bf7dc748410a385d6124b052b328a95
dgelessus <dgelessus@users.noreply.github.com>
2025-01-30 14:09:50 +0100

    Add test for cyclic refinement/name mismatch that wasn't detected before

--- bparser/src/test/java/de/be4/classicalb/core/parser/composition/ErrorsTest.java
+++ bparser/src/test/java/de/be4/classicalb/core/parser/composition/ErrorsTest.java
@@ -1,5 +1,6 @@
 package de.be4.classicalb.core.parser.composition;
 
+import de.be4.classicalb.core.parser.ParsingBehaviour;
 import de.be4.classicalb.core.parser.exceptions.CheckException;
 
 import org.junit.Test;
@@ -26,4 +27,15 @@ public class ErrorsTest {
 		assertEquals("Machine name does not match the file name: 'Foo' vs 'MachineNameDoesNotMatchFileName'", e.getMessage());
 	}
 
+	@Test
+	public void testCyclicRefinementWithMismatchedFileName() {
+		String file = "composition/errors/NFA_det.ref";
+		ParsingBehaviour parsingBehaviour = new ParsingBehaviour();
+		// machineNameMustMatchFileName = false only disables the check for the main machine.
+		// The names of referenced machines are always checked.
+		// Test that the mismatch is detected when the machine references itself in the REFINES clause.
+		parsingBehaviour.setMachineNameMustMatchFileName(false);
+		CheckException e = Helpers.assertThrowsCompound(CheckException.class, () -> Helpers.parseFile(file, parsingBehaviour));
+		assertEquals("Machine name does not match the file name: 'NFA_det1' vs 'NFA_det'", e.getMessage());
+	}
 }
