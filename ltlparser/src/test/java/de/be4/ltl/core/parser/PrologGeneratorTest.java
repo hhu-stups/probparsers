a315721b2f8b8a64261ebfe8470e3bc97bd1cd34
dgelessus <dgelessus@users.noreply.github.com>
2025-07-03 17:54:12 +0200

    Move DummyParser to top level for future use by multiple test classes

--- ltlparser/src/test/java/de/be4/ltl/core/parser/PrologGeneratorTest.java
+++ ltlparser/src/test/java/de/be4/ltl/core/parser/PrologGeneratorTest.java
@@ -12,9 +12,6 @@ import java.io.StringReader;
 import de.be4.ltl.core.ctlparser.lexer.Lexer;
 import de.be4.ltl.core.ctlparser.parser.Parser;
 import de.be4.ltl.core.ctlparser.parser.ParserException;
-import de.prob.parserbase.ProBParseException;
-import de.prob.parserbase.ProBParserBase;
-import de.prob.prolog.output.IPrologTermOutput;
 import de.prob.prolog.output.PrologTermStringOutput;
 
 import org.junit.Assert;
@@ -338,64 +335,5 @@ public class PrologGeneratorTest {
 		PrologTermStringOutput pto = new PrologTermStringOutput();
 		parser.printFormulaAsProlog(input, "root", pto);
 		return pto.toString();
-	}
-
-	private static class DummyParser implements ProBParserBase {
-		private final boolean suppPred, suppTransPred;
-
-		public DummyParser(final boolean suppPred, final boolean suppTransPred) {
-			this.suppPred = suppPred;
-			this.suppTransPred = suppTransPred;
-		}
-
-		@Override
-		public void parseExpression(final IPrologTermOutput pto,
-				final String expression, final boolean wrap)
-				throws ProBParseException {
-			throw new UnsupportedOperationException("no dummy expressions");
-		}
-
-		@Override
-		public void parsePredicate(final IPrologTermOutput pto,
-				final String predicate, final boolean wrap)
-				throws ProBParseException {
-			if (suppPred) {
-				parse(pto, predicate, wrap, "dpred");
-			} else {
-				throw new UnsupportedOperationException("no dummy predicates");
-			}
-		}
-
-		@Override
-		public void parseTransitionPredicate(final IPrologTermOutput pto,
-				final String transPredicate, final boolean wrap)
-				throws ProBParseException {
-			if (suppTransPred) {
-				parse(pto, transPredicate, wrap, "dtrans");
-			} else {
-				throw new UnsupportedOperationException(
-						"no dummy transition predicates");
-			}
-		}
-
-		private static void parse(final IPrologTermOutput pto, final String text,
-				final boolean wrap, final String wrapper)
-				throws ProBParseException {
-			// TODO: cant we use the real B parser here?
-			// Hardcoded cases for testing parse errors.
-			// Everything else is considered "successfully parsed".
-			if ("X".equals(text) || text.endsWith("{")) {
-				throw new ProBParseException("syntax error");
-			}
-
-			if (wrap) {
-				pto.openTerm(wrapper);
-			}
-			pto.printAtom(text);
-			if (wrap) {
-				pto.closeTerm();
-			}
-
-		}
 	}
 }
