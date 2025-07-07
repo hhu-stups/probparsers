e9e05e2c1205ad6b2754b19dc5bd1f56abb91a01
honoré <honore.marmion@student-cs.fr>
2025-07-07 16:50:35 +0200

    test

--- ltlparser/src/test/java/de/be4/ltl/core/parser/PctlParserTest.java
+++ ltlparser/src/test/java/de/be4/ltl/core/parser/PctlParserTest.java
@@ -11,7 +11,7 @@ public class PctlParserTest {
 	public void testTrue() throws Exception {
 		check("true", "true");
 	}
-	
+
 	@Test
 	public void testFalse() throws Exception {
 		check("false", "false");
@@ -21,6 +21,51 @@ public class PctlParserTest {
 	public void testImplication() throws Exception {
 		check("false =>   true ", "implies(false,true)");
 	}
+
+	@Test
+	public void testSink() throws Exception {
+		check("sink", "ap(sink)");
+	}
+
+	@Test
+	public void testDeadlock() throws Exception {
+		check("deadlock", "ap(deadlock)");
+	}
+
+	@Test
+	public void testCurrent() throws Exception {
+		check("current", "ap(stateid(root))");
+	}
+
+	@Test
+	public void testAnd() throws Exception {
+		check("true &  false", "and(true,false)");
+	}
+
+	@Test
+	public void testOr() throws Exception {
+		check("true or  false", "or(true,false)");
+	}
+
+	@Test
+	public void testNot() throws Exception {
+		check("not true", "not(true)");
+	}
+
+	@Test
+	public void testPredicate1() throws Exception {
+		check("{}", "");
+	}
+
+	@Test
+	public void testPredicate() throws Exception {
+		check("{blubb}", "ap(dpred(blubb))");
+	}
+/*
+	@Test
+	public void testNextLoop() throws Exception {
+		check("P={0.9} [X {p(b)}]", "formula_equal(0.9,x(p(b)))");
+	}*/
 
 	private static void check(String input, String expectedTerm) throws LtlParseException {
 		String term = parse(input);
