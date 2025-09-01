92fe451ced1d5e8e840c3a5891698a4c7a53845b
dgelessus <dgelessus@users.noreply.github.com>
2025-09-01 16:29:18 +0200

    Fix indents

--- ltlparser/src/main/java/de/be4/ltl/core/parser/internal/PrologPctlGenerator.java
+++ ltlparser/src/main/java/de/be4/ltl/core/parser/internal/PrologPctlGenerator.java
@@ -73,16 +73,16 @@ public class PrologPctlGenerator extends DepthFirstAdapter {
 	public void caseAProbFormulaPctlState(AProbFormulaPctlState node) {
 		inAProbFormulaPctlState(node);
 		if(node.getOp() != null)
-        {
-            node.getOp().apply(this);
-        }
+		{
+			node.getOp().apply(this);
+		}
 		final Token token = node.getProbability();
 		helper.caseUnparsedExpression(UniversalToken.createToken(token),false);
 		if(node.getCont() != null)
-        {
-            node.getCont().apply(this);
-        }
-        outAProbFormulaPctlState(node);
+		{
+			node.getCont().apply(this);
+		}
+		outAProbFormulaPctlState(node);
 	}
 
 	@Override
